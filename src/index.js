111f22929492b853aea0cb650beac1f0af611d0f
Conduitry <git@fur.army>
2018-04-20 09:14:44 -0400

    support specifying files with arrays/buffers

--- src/index.js
+++ src/index.js
@@ -10,34 +10,36 @@ for (let n = 0; n < 256; n++) {
 	crcTable[n] = c;
 }
 
-const strCRC = str => {
-	let crc = -1;
-	for (let i = 0; i < str.length; i++) {
-		crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+const crc32 = bytes => {
+	let sum = -1;
+	for (const byte of bytes) {
+		sum = (sum >>> 8) ^ crcTable[(sum ^ byte) & 0xFF];
 	}
-	return crc ^ -1;
+	return sum ^ -1;
 };
 
-const intToBytes = (int, length) => {
+const int = (n, length) => {
 	const out = [];
 	while (length--) {
-		out.push(int & 0xFF);
-		int >>>= 8;
+		out.push(n & 0xFF);
+		n >>>= 8;
 	}
 	return out;
 };
 
-const strToBytes = str => [...str].map(char => char.charCodeAt(0));
+const toBytes = data => typeof data === 'string' ? [...data].map(char => char.charCodeAt(0)) : data;
 
 export default files => {
 	const fileData = [];
 	const centralDirectory = [];
 	for (const { path, data } of files) {
-		const commonHeader = [0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, ...intToBytes(strCRC(data), 4), ...intToBytes(data.length, 4), ...intToBytes(data.length, 4), ...intToBytes(path.length, 2), 0x00, 0x00];
-		centralDirectory.push(0x50, 0x4B, 0x01, 0x02, 0x14, 0x00, ...commonHeader, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, ...intToBytes(fileData.length, 4), ...strToBytes(path));
-		fileData.push(0x50, 0x4B, 0x03, 0x04, ...commonHeader, ...strToBytes(path), ...strToBytes(data));
+		const dataBytes = toBytes(data);
+		const pathBytes = toBytes(path);
+		const commonHeader = [0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, ...int(crc32(dataBytes), 4), ...int(dataBytes.length, 4), ...int(dataBytes.length, 4), ...int(pathBytes.length, 2), 0x00, 0x00];
+		centralDirectory.push(0x50, 0x4B, 0x01, 0x02, 0x14, 0x00, ...commonHeader, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, ...int(fileData.length, 4), ...pathBytes);
+		fileData.push(0x50, 0x4B, 0x03, 0x04, ...commonHeader, ...pathBytes, ...dataBytes);
 	}
-	const bytes = [...fileData, ...centralDirectory, 0x50, 0x4B, 0x05, 0x06, 0x00, 0x00, 0x00, 0x00, ...intToBytes(files.length, 2), ...intToBytes(files.length, 2), ...intToBytes(centralDirectory.length, 4), ...intToBytes(fileData.length, 4), 0x00, 0x00];
+	const bytes = [...fileData, ...centralDirectory, 0x50, 0x4B, 0x05, 0x06, 0x00, 0x00, 0x00, 0x00, ...int(files.length, 2), ...int(files.length, 2), ...int(centralDirectory.length, 4), ...int(fileData.length, 4), 0x00, 0x00];
 	if (typeof Blob !== 'undefined' && typeof Uint8Array !== 'undefined') {
 		return new Blob([Uint8Array.from(bytes)], { type: 'application/zip' });
 	}
